8ba11184e4f08bd8413897468daa8911837da98b
Marie-zaj <mzajka544@gmail.com>
2026-03-27 16:29:11 +0200

    Add files via upload

--- Server2.cpp
+++ Server2.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <winsock2.h>
 #include <vector>
-#include <string>
 #include <thread>
 #include <mutex>
 #include <ctime>
@@ -10,8 +9,7 @@
 #pragma comment(lib, "ws2_32.lib")
 using namespace std;
 
-struct ClientInfo
-{
+struct ClientInfo {
     string login;
     string password;
     string ip;
@@ -22,77 +20,53 @@ struct ClientInfo
 vector<ClientInfo> clients;
 mutex mtx;
 
-string getTime()
-{
+string getTime() {
     time_t now = time(0);
     tm t;
     localtime_s(&t, &now);
-    char buf[20];
+    char buf[10];
     strftime(buf, sizeof(buf), "%H:%M:%S", &t);
     return string(buf);
 }
 
-void broadcast(string msg)
-{
+void broadcast(string msg) {
     lock_guard<mutex> lock(mtx);
     for (auto& c : clients)
-    {
         send(c.socket, msg.c_str(), msg.size(), 0);
-    }
 }
 
-void handleClient(SOCKET sock, sockaddr_in addr)
-{
+void handleClient(SOCKET sock, sockaddr_in addr) {
     char buffer[512];
 
-    int bytes = recv(sock, buffer, 512, 0);
-    string auth(buffer, bytes);
+    int len = recv(sock, buffer, 512, 0);
+    string auth(buffer, len);
 
     string login = auth.substr(0, auth.find(':'));
     string pass = auth.substr(auth.find(':') + 1);
 
-    string ip = inet_ntoa(addr.sin_addr);
-    int port = ntohs(addr.sin_port);
-
     {
         lock_guard<mutex> lock(mtx);
-        bool found = false;
-
-        for (auto& c : clients)
-        {
-            if (c.login == login && c.password == pass)
-                found = true;
-        }
-
-        if (!found)
-            clients.push_back({ login, pass, ip, port, sock });
+        clients.push_back({ login, pass, "", 0, sock });
     }
 
-    string msg = login + " connected [" + getTime() + "]\n";
-    cout << msg;
-    broadcast(msg);
+    string join = login + " connected\n";
+    broadcast(join);
 
-    while (true)
-    {
-        int len = recv(sock, buffer, 512, 0);
-        if (len <= 0) break;
+    while (true) {
+        int bytes = recv(sock, buffer, 512, 0);
+        if (bytes <= 0) break;
 
-        string text(buffer, len);
-        string full = login + ": " + text + " [" + getTime() + "]\n";
+        string msg(buffer, bytes);
+        string full = "[" + getTime() + "] " + login + ": " + msg + "\n";
 
         cout << full;
         broadcast(full);
     }
 
-    string left = login + " disconnected [" + getTime() + "]\n";
-    cout << left;
-    broadcast(left);
-
     closesocket(sock);
 }
 
-int main()
-{
+int main() {
     WSAData wsa;
     WSAStartup(MAKEWORD(2, 2), &wsa);
 
@@ -106,14 +80,11 @@ int main()
     bind(server, (sockaddr*)&addr, sizeof(addr));
     listen(server, 5);
 
-    cout << "Server started...\n";
-
-    while (true)
-    {
+    while (true) {
         sockaddr_in clientAddr;
         int size = sizeof(clientAddr);
-
         SOCKET client = accept(server, (sockaddr*)&clientAddr, &size);
+
         thread(handleClient, client, clientAddr).detach();
     }
 }
