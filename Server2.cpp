b278146d326fdfdbc221b76aff1a9af118171ec2
Marie-zaj <mzajka544@gmail.com>
2026-03-27 17:10:11 +0200

    Add files via upload

--- Server2.cpp
+++ Server2.cpp
@@ -1,10 +1,12 @@
 ﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
 #include <iostream>
 #include <winsock2.h>
 #include <vector>
 #include <thread>
 #include <mutex>
 #include <fstream>
+#include <ctime>
 
 #pragma comment(lib, "ws2_32.lib")
 using namespace std;
@@ -12,8 +14,6 @@ using namespace std;
 struct ClientInfo {
     string login;
     string password;
-    string ip;
-    int port;
     int color;
     SOCKET socket;
 };
@@ -21,25 +21,42 @@ struct ClientInfo {
 vector<ClientInfo> clients;
 mutex mtx;
 
-void handleClient(SOCKET sock, sockaddr_in addr) {
+string getTime() {
+    time_t now = time(0);
+    tm t;
+    localtime_s(&t, &now);
+    char buf[10];
+    strftime(buf, sizeof(buf), "%H:%M:%S", &t);
+    return string(buf);
+}
+
+void broadcast(string msg) {
+    lock_guard<mutex> lock(mtx);
+    for (auto& c : clients) {
+        send(c.socket, msg.c_str(), msg.size(), 0);
+    }
+}
+
+void handleClient(SOCKET sock) {
     char buffer[512];
 
+    // авторизация
     int len = recv(sock, buffer, 512, 0);
-    string auth(buffer, len);
+    if (len <= 0) return;
 
+    string auth(buffer, len);
     string login = auth.substr(0, auth.find(':'));
     string pass = auth.substr(auth.find(':') + 1);
 
     int color;
     recv(sock, (char*)&color, sizeof(color), 0);
 
+    // проверка файла
     bool returning = false;
     ifstream in("clients.txt");
 
-    string l, p, ipf;
-    int portf;
-
-    while (in >> l >> p >> ipf >> portf) {
+    string l, p;
+    while (in >> l >> p) {
         if (l == login && p == pass)
             returning = true;
     }
@@ -47,7 +64,7 @@ void handleClient(SOCKET sock, sockaddr_in addr) {
 
     if (!returning) {
         ofstream out("clients.txt", ios::app);
-        out << login << " " << pass << " 0 0\n";
+        out << login << " " << pass << "\n";
         out.close();
     }
     else {
@@ -57,19 +74,27 @@ void handleClient(SOCKET sock, sockaddr_in addr) {
 
     {
         lock_guard<mutex> lock(mtx);
-        clients.push_back({ login, pass, "", 0, color, sock });
+        clients.push_back({ login, pass, color, sock });
     }
+
+    string join = login + " connected [" + getTime() + "]\n";
+    cout << join;
+    broadcast(join);
 
     while (true) {
         int bytes = recv(sock, buffer, 512, 0);
         if (bytes <= 0) break;
 
         string msg(buffer, bytes);
-        string full = login + ": " + msg + "\n";
+        string full = "[" + getTime() + "] " + login + ": " + msg + "\n";
 
-        for (auto& c : clients)
-            send(c.socket, full.c_str(), full.size(), 0);
+        cout << full;
+        broadcast(full);
     }
+
+    string left = login + " disconnected [" + getTime() + "]\n";
+    cout << left;
+    broadcast(left);
 
     closesocket(sock);
 }
@@ -88,11 +113,10 @@ int main() {
     bind(server, (sockaddr*)&addr, sizeof(addr));
     listen(server, 5);
 
-    while (true) {
-        sockaddr_in clientAddr;
-        int size = sizeof(clientAddr);
-        SOCKET client = accept(server, (sockaddr*)&clientAddr, &size);
+    cout << "Server started...\n";
 
-        thread(handleClient, client, clientAddr).detach();
+    while (true) {
+        SOCKET client = accept(server, NULL, NULL);
+        thread(handleClient, client).detach();
     }
 }
